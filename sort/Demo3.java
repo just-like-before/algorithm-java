97939c12ce47326a65032b18eba915cd29de31e0
Your Name <you@example.com>
2019-01-29 23:01:23 +0800

    排序

---
+++ sort/Demo3.java
@@ -0,0 +1,18 @@
+package com.algorithm.sort;
+
+/**
+ * 插入排序
+ *
+ *
+ */
+public class Demo3 {
+
+    public static void main(String[] args) {
+        int[] a = {9,2,5,8,4,7,6,1};
+        run1(a);
+    }
+
+    public static void run1(int[] a){
+
+    }
+}
