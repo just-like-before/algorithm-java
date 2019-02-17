8e936bcc5d902005955044d4074bd27fb77a8446
Your Name <you@example.com>
2019-02-17 23:19:01 +0800

    动态代理

---
+++ basics/demo2/StudentDao.java
@@ -0,0 +1,8 @@
+package com.algorithm.basics.demo2;
+
+public interface StudentDao {
+
+    void addStudent();
+
+    void deleteStudent();
+}
---
+++ basics/demo2/Text.java
@@ -0,0 +1,13 @@
+package com.algorithm.basics.demo2;
+
+import java.util.HashMap;
+
+public class Text {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer,String> hashMap = new HashMap<>();
+        hashMap.put(1,"1");
+
+    }
+}
